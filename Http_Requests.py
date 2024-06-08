27f4ae0a6c565c5e8c117acd7daa467f23877e3b
Jaxon H <jaxonthgit@gmail.com>
2024-06-08 17:20:25 -0400

    init

--- Http_Requests.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://api.github.com/users/python"
-try:
-    response = requests.get(url)
-    print(response.text)
-except:
-    print("No internet connectivity.")
--- Number_of_secs_in_a_year.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print(60 * 60 * 24 * 365)
-
-if __name__ == "__main__":
-    main()
